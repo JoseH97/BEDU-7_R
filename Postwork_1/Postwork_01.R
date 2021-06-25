3ff223a452db4c9891f24e629ef55f4952f4a349
unknown <equilibrio.top15@gmail.com>
2021-06-25 13:58:23 -0500

    Lectura de datos y extracción de columnas en la función read.csv

--- Postwork_1/Postwork_01.R
+++ Postwork_1/Postwork_01.R
@@ -1,9 +1,14 @@
 # 1.1 Importa los datos de soccer de la temporada 2019/2020 de la primera división de la liga española
-df.liga <- read.csv("./data/SP1.csv")
+# df.liga <- read.csv("./data/SP1.csv")
 
-# 1.2. Extrae las columnas que contienen los números de goles anotados por los equipos que jugaron en casa (FTHG) 
+# 1.2. Extrae las columnas que contienen los números de goles anotados por los equipos que jugaron en casa (FTHG)
 #    y los goles anotados por los equipos que jugaron como visitante (FTAG)
-(golesAnotados <- df.liga[ ,c("FTHG","FTAG")])
+# (golesAnotados <- df.liga[ ,c("FTHG","FTAG")]) opcional, es mas rapido hacer el procesamiento en read.csv
+
+golesAnotados <- read.csv("./data/SP1.csv",
+                 colClasses = c(rep("NULL",5),rep("integer",2),rep("NULL",98)))
+
+
 
 # 1.3. Consulta cómo funciona la función table en R al ejecutar en la consola ?table
 (t.goles <- table("Local"= golesAnotados$FTHG,"Visitante"=golesAnotados$FTAG))
@@ -18,7 +23,7 @@ df.liga <- read.csv("./data/SP1.csv")
 (visitante <- colSums(t.goles))
 (visitante.prob <- round(visitante/sum(visitante),3))
 
-# 1.3.3 La probabilidad (conjunta) de que el equipo que juega en casa anote x goles y 
+# 1.3.3 La probabilidad (conjunta) de que el equipo que juega en casa anote x goles y
 # el equipo que juega como visitante anote y goles (x = 0, 1, 2, ..., y = 0, 1, 2, ...)
 
 round(t.goles/sum(t.goles),3)
