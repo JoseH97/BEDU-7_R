394e3cb99df5c64f5d0fb59e52533e05010b7e35
unknown <equilibrio.top15@gmail.com>
2021-06-25 14:25:01 -0500

    agregamos la opción de descragar el conjunto de datos desde la web

--- Postwork_1/Postwork_01.R
+++ Postwork_1/Postwork_01.R
@@ -5,18 +5,26 @@
 #    y los goles anotados por los equipos que jugaron como visitante (FTAG)
 # (golesAnotados <- df.liga[ ,c("FTHG","FTAG")]) opcional, es mas rapido hacer el procesamiento en read.csv
 
+
+# Descargamos los datos del servidor si no los tenemos
+if (file.exists("./data/SP1.csv") == FALSE){
+    url <- "https://www.football-data.co.uk/mmz4281/1920/SP1.csv"
+    download.file(url = url, destfile = "./data/SP1.csv", mode = "wb")
+}
+
 golesAnotados <- read.csv("./data/SP1.csv",
                  colClasses = c(rep("NULL",5),rep("integer",2),rep("NULL",98)))
 
 
 
 # 1.3. Consulta cómo funciona la función table en R al ejecutar en la consola ?table
-(t.goles <- table("Local"= golesAnotados$FTHG,"Visitante"=golesAnotados$FTAG))
+t.goles <- table("Local"= golesAnotados$FTHG,"Visitante"=golesAnotados$FTAG)
 
 # 1.3.1 La probabilidad (marginal) de que el equipo que juega en casa anote x goles (x = 0, 1, 2, ...)
 
 (local <- rowSums(t.goles))
 (local.prob <- round(local/sum(local),3))
+
 
 # 1.3.2 La probabilidad (marginal) de que el equipo que juega como visitante anote y goles (y = 0, 1, 2, ...)
 
@@ -26,5 +34,5 @@ golesAnotados <- read.csv("./data/SP1.csv",
 # 1.3.3 La probabilidad (conjunta) de que el equipo que juega en casa anote x goles y
 # el equipo que juega como visitante anote y goles (x = 0, 1, 2, ..., y = 0, 1, 2, ...)
 
-round(t.goles/sum(t.goles),3)
+(conjunta.prob <- round(t.goles/sum(t.goles),3))
 
