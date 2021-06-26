46bb24c00efe445ec5d1d0444138c14c7039e370
unknown <equilibrio.top15@gmail.com>
2021-06-26 11:23:48 -0500

    Parte dos completada. Con técnica Boostrop

--- Postwork_4/PostWork4.R
+++ Postwork_4/PostWork4.R
@@ -40,6 +40,78 @@ marginalProd <- local.prob %o% visitante.prob
 # la tabla en el punto 1, son iguales a 1 (en tal caso tendríamos independencia
 # de las variables aleatorias X y Y).
 
+# La técnica de boostrap es una técnica de remuestreo y sirve para
+# estimar estadisticos de una población (como la media,varianza,correlacion, etc...)
+
+# Esta ténica la vamos a utilizar para hacer un remuestreo de los goles de local
+# y los goles vistante, para despues cálcular los coeficientes p(x_i,y_j)/(p(x_i)*p(y_j))
+# en cada muestra.
+
+# Defino los parámetros del boostrap
+k <- 10 # numero de submuestreos
+n <- dim(golesAnotados)[1] # cantidad de datos que se van a remuestrear en una muestra
+
+
+# Paso 1: calcular los coefiecientes para cada muestra
+
+t.golesBoostrap <- replicate(k, {
+    # muestreamos con remplezo de goles de local y visitante
+    golesAnotados.perSample <- data.frame("FTHG" = sample(golesAnotados$FTHG,n,replace = FALSE),
+                                          "FTAG" = sample(golesAnotados$FTAG,n,replace = FALSE))
+
+    # obtenemos las frecuencias relativas
+    t.golesPerSample <- table("Local" = golesAnotados.perSample$FTHG,
+                              "Visitante" = golesAnotados.perSample$FTAG)
+
+    # Obtenemos la distribucion de probabilidad marginal de locales
+    local.PerSample <- rowSums(t.golesPerSample)
+    local.probPerSample <- round(local.PerSample/sum(local.PerSample),3)
+
+
+    # Creamos la distribucion de probabilidad marginal de visitantes
+    visitante.PerSample <- colSums(t.golesPerSample)
+    visitante.probPerSample <- round(visitante.PerSample/sum(visitante.PerSample),3)
+
+    # Creamos la distribucion de probabilidad conjunta
+    conjunta.probPerSample <- round(t.golesPerSample/sum(t.golesPerSample),3)
+
+
+    # Calculamos la matriz de multiplicacion de probabilidades marginales respectivas
+    # Utilizamos el outer product para generar la matriz
+    marginalProd.PerSample <- local.probPerSample %o% visitante.probPerSample
+
+
+    # Calculamos los coeficientes : p(x_i,y_j)/(p(x_i)*p(y_j))
+    coefTable.PerSample <- conjunta.probPerSample/marginalProd.PerSample
+
+
+    coefTable.PerSample
+
+})
+
+
+# Paso 2: Calcular la media y sd de cada coeficiente sobre cada matriz de coeficientes
+coefTable.BoostrapMean <- apply(t.golesBoostrap,c(1,2) ,mean) # Estimamos p(x_i,y_j)/(p(x_i)*p(y_j))
+coefTable.BoostrapSd <- apply(t.golesBoostrap,c(1,2) ,sd) # error de la estimacion
+
+# Mostramos los resultados
+print(coefTable.BoostrapMean)
+print(coefTable.BoostrapSd)
+
+
+# Con base en los resultados mostrados en coefTable.BoostrapMean (boostrapCoef.jpg)
+# Variables independientes :
+# (Local=0,Visitante=0), (Local=1,Visitante=0), (Local=2,Visitante=0) ,(Local=3,Visitante=0)
+# (Local=0,Visitante=1), (Local=1,Visitante=1), (Local=2,Visitante=1)
+
+# Variables dependientes : son las variables que sobran
+
+# El criterio para ver la dependencia es que la estimacion de Estimamos p(x_i,y_j)/(p(x_i)*p(y_j))
+# se cercano a 1 +- 0.09, si la desviacion estandar es mayor que 0.1 no se toma
+
+
+
+
 
 
 
