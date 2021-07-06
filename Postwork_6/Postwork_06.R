ace717c36eb8f81ad1ef8c54f29f94800873c8b9
unknown <equilibrio.top15@gmail.com>
2021-07-06 13:58:15 -0500

    Comentarios relacionados a la conclucion de la gráfica serieTiempo.png

--- Postwork_6/Postwork_06.R
+++ Postwork_6/Postwork_06.R
@@ -22,14 +22,20 @@ matchData <- matchData %>% mutate(sumaGoles = home.score + away.score)
 
 # 2.- Obtén el promedio por mes de la suma de goles.
 matchData <- matchData %>% group_by(año = year(date) ,mes = month(date)) %>%
-                           summarise(promedioSumaGoles = mean(sumaGoles)) %>%
-                           filter(año != "2020")
+                           summarise(promedioSumaGoles = mean(sumaGoles))
 
 # 3.- Crea la serie de tiempo del promedio por mes de la suma de goles hasta diciembre de 2019.
-ts_matchData <- ts(matchData[,3],start = c(2010,8),frequency = 10)
+ts_matchData <- ts(matchData[,3],start = c(2010,8), end = c(2019,12),frequency = 10)
 
 
 # 4.- Grafica la serie de tiempo
 plot(ts_matchData, xlab = "Tiempo", ylab = "Promedio de la suma de goles por mes", main = "Serie tiempo de match.data.cvs",
      sub = "Serie mensual: Agosto de 2010 a Diciembre de 2019")
 
+
+# Debido a la naturaleza de la gráfica, no se observa una variacion estacional. 
+# Entonces no es apropiado descomponer la serie ni multiplicativamente ni aditivamente. 
+
+# La informacion que podemos obtener de la gráfica es que a finales del año 2014 se anotaron 
+# mas goles con un promedio de mes mayor a 3.5 y a finales del año 2016 se anotaron menos goles 
+# con un promedio menor a 2.0.
